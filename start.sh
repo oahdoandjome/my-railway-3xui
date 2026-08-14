9cdada5c0e88425e9a83364fd207d488265bc74e
oahdoandjome <jomaamaaa@atomicmail.io>
2026-08-14 04:49:43 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 export PORT=${PORT:-2053}
+export XUI_PORT=2052
 
 # Render nginx config with Railway's public PORT
 envsubst '$PORT' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# Start Nginx reverse proxy
+# Start Nginx
 nginx
 
-# Start 3x-ui panel
+# Start 3x-ui
 exec /app/x-ui
